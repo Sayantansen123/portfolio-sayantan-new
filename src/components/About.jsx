c2f76d79b9f330cc0fc715d3f0fda724c53975da
Sayantansen123 <riosayan123@gmail.com>
2025-07-03 00:59:46 +0530

    updated 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -29,33 +29,33 @@ const About = () => {
         <div className='w-[40%] min-w-[290px] '>
           <Info />
           <p className='text-sm font-light pb-5'>I'm Sayantan Sen, a web developer passionate about building scalable and user-friendly applications using React.js, Next.js, Node.js, and Express. I’ve worked on projects like Auraverse, a gamified web app promoting social awareness, a real-time chat application, a real estate platform, a web scraping API, and a video streaming platform. Constantly exploring new technologies, I love turning ideas into functional, impactful solutions. Let's build something great together! 🚀</p>
-
-          <button
-            className=" text-center  w-40 rounded-2xl h-12 relative text-black text-[1rem] font-semibold group"
-            type="button"
-          >
-            <div
-              className="bg-black rounded-xl h-10 w-1/4 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500"
+          <a href="/resume.pdf" download>
+            <button
+              className=" text-center  w-40 rounded-2xl h-12 relative text-black text-[1rem] font-semibold group"
+              type="button"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 1024 1024"
-                height="20px"
-                width="20px"
+              <div
+                className="bg-black rounded-xl h-10 w-1/4 flex items-center justify-center absolute left-1 top-[4px] group-hover:w-[184px] z-10 duration-500"
               >
-                <path
-                  d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
-                  fill="#ffff"
-                ></path>
-                <path
-                  d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
-                  fill="#ffff"
-                ></path>
-              </svg>
-            </div>
-            <p className="translate-x-2 ">Download CV</p>
-          </button>
-
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 1024 1024"
+                  height="20px"
+                  width="20px"
+                >
+                  <path
+                    d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"
+                    fill="#ffff"
+                  ></path>
+                  <path
+                    d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"
+                    fill="#ffff"
+                  ></path>
+                </svg>
+              </div>
+              <p className="translate-x-2 ">Download CV</p>
+            </button>
+          </a>
         </div>
 
       </motion.div>
